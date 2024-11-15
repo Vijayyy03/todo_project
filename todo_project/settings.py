2c88d4e040e74dc5c8fc429c59d276ae7e951eca
Vijay <jagdalevijay92@gmail.com>
2024-11-15 20:02:41 +0530

    Again

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^1-6t-z-qex$s#ggkt7tv$06it0n4_%bsvoyqmy!$zjs%!0o$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://todo-project-bb03.onrender.com']
 
 
 # Application definition
